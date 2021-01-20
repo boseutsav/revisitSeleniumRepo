17767969641d15e03b08f1d3b5711c087503e774
boseutsav <utsav050489@gmail.com>
2021-01-20 14:39:15 +0530

    modified comment

--- RevisitSelenium/src/packageOneSel/edurekaScript.java
+++ RevisitSelenium/src/packageOneSel/edurekaScript.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
- * @author Utsav
+ * @author Utsav Bose
  *
  */
 public class edurekaScript {
